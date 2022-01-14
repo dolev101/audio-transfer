5960a4fac6ec6c2b85d971871ba8083a3b7ba660
dolev <dolev@dolevc.com>
2022-01-14 16:55:38 +0200

    established connection with the server

--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 import socket, pyaudio, wave, argparse, queue, threading
 
 # HOST = "192.168.43.118"
-from time import sleep
-
 HOST = "localhost"
 PORT = 5634
 CHUNK = 1024
@@ -14,22 +12,21 @@ WAVE_OUT_FILENAME = "output.wav"
 BUFFER = 10 * 1024
 
 
-class Client:
+class Server:
     def __init__(self, server_hostname: str = HOST, port: int = PORT):
         self.server_hostname = server_hostname
         self.port = port
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFFER)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.audio_manager = pyaudio.PyAudio()
-        self.queue = queue.Queue()
 
     def __enter__(self):
+        self.server_socket.bind((self.server_hostname, self.port))
         self.stream = self.audio_manager.open(format=FORMAT, channels=CHANNELS, rate=RATE,
                                               input=True, frames_per_buffer=CHUNK)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.client_socket.close()
+        self.server_socket.close()
 
     # def get_frames(self):
     #     frames = []
@@ -39,25 +36,15 @@ class Client:
     #         frames.append(data)
     #     return frames
     def start(self):
-        queue_add_thread = threading.Thread(target=self.add_frame)
-        queue_pop_thread = threading.Thread(target=self.send_frames)
-        queue_pop_thread.start()
-        queue_add_thread.start()
-        sleep(10000)
 
-    def add_frame(self):
         while True:
-            self.queue.put(self.stream.read(CHUNK))
-
-    def send_frames(self):
-        while True:
-            data = self.queue.get()
-            self.client_socket.sendto(data, (HOST, PORT))
+            msg, client_address = self.server_socket.recvfrom(BUFFER)
+            print(msg)
 
 
 def main():
-    with Client(HOST, PORT) as client:
-        client.start()
+    with Server(HOST, PORT) as server:
+        server.start()
 
 
 if __name__ == "__main__":
